de68d1596cf2bfb89afa63f9040c449c8993d5e2
vinjas <danielvinyastorres@gmail.com>
2021-07-05 14:47:22 +0200

    Basic App and Robots components

--- robots-tour/src/app/app.module.ts
+++ robots-tour/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RobotsComponent } from './robots/robots.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RobotsComponent
   ],
   imports: [
     BrowserModule
---
+++ robots-tour/src/app/robots/robots.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-robots',
+  templateUrl: './robots.component.html',
+  styleUrls: ['./robots.component.css']
+})
+export class RobotsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  robot = "Windstorm" ;
+}
